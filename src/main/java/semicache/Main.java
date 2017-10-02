34c1a16230294961f18c962c771b486bc1c3fd11
Przemysław Konik <przemyslawkonik@gmail.com>
2017-10-02 15:30:13 +0200

    added test in main method

--- src/main/java/semicache/Main.java
+++ src/main/java/semicache/Main.java
@@ -1,18 +1,23 @@
 package semicache;
 
-import semicache.api.CacheItem;
+
+import semicache.api.Cache;
 import semicache.impl.CacheImpl;
 import semicache.impl.Item;
 
 public class Main {
+
     public static void main(String[] args) {
-        CacheImpl cache = new CacheImpl(3);
+        Cache cache = new CacheImpl(3);
+        String[] tab = {"A", "B", "C", "D"};
 
-        cache.putItem("A",cache.cacheItem(null, "A"));
+        for(String t : tab) {
+            cache.cacheItem(new Item(t,t),t);
 
-        cache.getCacheView();
-
-
-
+            for(int i=0; i<cache.getView().size(); i++) {
+                System.out.print(cache.getView().getItem(i).getValue() + "  ");
+            }
+            System.out.println();
+        }
     }
 }
