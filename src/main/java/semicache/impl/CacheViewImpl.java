7e6ae836b5c56b9ed7d8776cb42291e2683112d8
Przemysław Konik <przemyslawkonik@gmail.com>
2017-10-02 15:12:41 +0200

    added cache view implementation

---
+++ src/main/java/semicache/impl/CacheViewImpl.java
@@ -0,0 +1,30 @@
+package semicache.impl;
+
+import semicache.api.CacheItem;
+import semicache.api.CacheView;
+
+import java.util.HashMap;
+
+/**
+ * Created by Przemysław Konik on 2017-10-02.
+ */
+public class CacheViewImpl implements CacheView {
+
+    private HashMap<String, CacheItem> cache;
+
+    public CacheViewImpl(HashMap<String, CacheItem> cache) {
+        this.cache = cache;
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+    public CacheItem getItem(int index) {
+        return (CacheItem) cache.values().toArray()[index];
+    }
+
+    public CacheItem getItem(String key) {
+        return cache.get(key);
+    }
+}
