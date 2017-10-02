8a741e759030ca72a693fc6d218867f42f00cdd0
Przemysław Konik <przemyslawkonik@gmail.com>
2017-10-02 15:10:28 +0200

    changed cache item implementation

--- src/main/java/semicache/impl/Item.java
+++ src/main/java/semicache/impl/Item.java
@@ -3,8 +3,9 @@ package semicache.impl;
 import semicache.api.CacheItem;
 
 public class Item implements CacheItem {
-    String key;
-    Object value;
+
+    private String key;
+    private Object value;
 
     public Item(String key, Object value) {
         this.key = key;
@@ -15,15 +16,7 @@ public class Item implements CacheItem {
         return key;
     }
 
-    public void setKey(String key) {
-        this.key = key;
-    }
-
     public Object getValue() {
         return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
     }
 }
